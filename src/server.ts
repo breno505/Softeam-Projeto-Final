f6a5db6350bd268dfc9df427639fe6667e16779a
Breno <91578149+breno505@users.noreply.github.com>
2024-02-06 09:37:49 -0300

    Feat(routes.ts, server.ts)
    Adicionei a primeira rota de teste.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { routes } from './routes';
 
 const app = Fastify({logger: true});
 
 const start = async ( )=> {
+    await app.register(cors);
+    await app.register(routes);
     try {
         await app.listen({port: 3333})
     }catch(err) {
